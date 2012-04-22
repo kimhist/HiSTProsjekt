066f02f01e9318675d37b4bae83dc0dccf582100
Svein E. Gripsgaard <kontakt@gripsgaard.no>
2012-04-22 20:00:40 +0200

    Startet skjema for tatabase test

---
+++ Program/frmDatabasetest.vb
@@ -0,0 +1,10 @@
+﻿Public Class frmDatabasetest
+
+
+    Private Sub btnTest_Click(sender As System.Object, e As System.EventArgs) Handles btnTest.Click
+        Dim oppkobling As New Sql("db.stud.aitel.hist.no", "database", "brukernavn", "passord")
+
+
+
+    End Sub
+End Class
