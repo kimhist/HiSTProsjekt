177df6fe659ae60a99f2c37640a9f7dedbbf70b1
Svein E. Gripsgaard <kontakt@gripsgaard.no>
2012-04-25 11:45:43 +0200

    Skjema for å teste opkobling mot databasen

--- Program/frmDatabasetest.vb
+++ Program/frmDatabasetest.vb
@@ -2,8 +2,11 @@
 
 
     Private Sub btnTest_Click(sender As System.Object, e As System.EventArgs) Handles btnTest.Click
-        Dim oppkobling As New Sql("db.stud.aitel.hist.no", "database", "brukernavn", "passord")
+        Dim oppkobling As New Sql("mysql.stud.aitel.hist.no", "ib-gr2", "ib-gr2", "M9w2rFye")
 
+        If oppkobling.kobleTilOK Then
+            MsgBox("Oppkoblingen gikk veldig bra")
+        End If
 
 
     End Sub
